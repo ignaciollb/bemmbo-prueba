fcab47d18094d1478fb6e02beaa5f8b184e078bf
ignaciollb <ignaciollanosberrocal@gmail.com>
2025-07-04 13:03:57 -0400

    feat: invoices list table

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import bemmboLogo from "./assets/bemmbo-logo.svg";
+import InvoicesList from "./InvoicesList";
 
 function App() {
-  return (
-    <>
-      {/* Build your page here */}
-      <div className="flex justify-center items-center h-screen w-screen bg-slate-50">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <img src={bemmboLogo} alt="Bemmbo Logo" className="w-1/2 h-1/2" />
-          <h1 className="text-3xl font-bold text-blue-600">
-            Prueba técnica Bemmbo
-          </h1>
-        </div>
-      </div>
-    </>
-  );
+  return <InvoicesList />;
 }
 
 export default App;
